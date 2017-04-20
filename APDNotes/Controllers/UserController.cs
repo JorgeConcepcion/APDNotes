d8abf6f39a87d0aa24e391a717995c5509b54e1e
Jorge Concepcion <jorge.concep@yahoo.com>
2017-04-20 15:50:59 -0400

    Now we ca run it on browser
    Created a Login Basic view and cutomized the Layout
    Now we can run it on a browser

---
+++ APDNotes/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using APDNotes.Model;
+
+namespace APDNotes.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(string username)
+        {
+            return View();
+        }
+
+        public ActionResult Writer()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Writer(string username)
+        {
+            
+            return View();
+        }
+        
+        public ActionResult Checker()
+        {
+            ViewBag.Note = new List<Note>();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Checker(string username)
+        {
+            return View();
+        }
+
+      
+
+    }
+}
